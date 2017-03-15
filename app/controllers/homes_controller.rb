9e28f15d3bde548c6081c1a10dd1299377e918a9
Enoch Ko <enoch232@gmail.com>
2017-03-15 12:56:34 -0700

    add bootstrap, and template

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,5 @@
 class HomesController < ApplicationController
+  def landing_page
+    render 'homes/landing-page'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/landing-page' => 'homes#landing_page'
+  post '/users' => 'users#create'
+  root 'homes#landing_page'
 end
