497d63216d21bce3c3a11daf825e5659bb788767
Enoch Ko <enoch232@gmail.com>
2017-03-15 19:16:21 -0700

    add subscription

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
   def landing_page
+    @user = User.new
+    flash[:notice] = ""
     render 'homes/landing-page'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Subscribed Successfully!"
+      render 'homes/landing-page'
+    else
+      flash[:notice] = "Something went wrong!"
+      render 'homes/landing-page'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
